6150546e28db3a4ce027ed6b2e88e841ec10b10c
jyoshitanair <jyoshitanair123@gmail.com>
2026-06-11 18:54:41 -0700

    commiting my header page!

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,32 +1,11 @@
 import { createRoot } from 'react-dom/client'
-class Car{
-  constructor(name){
-    this.brand = name;
-  }
-}
-function GetCar(){
-  const mycar = new Car("bilbo");
-  return <h1>{mycar.brand}</h1>;
-}
+import './index.css'
 const element = (
-  <table>
-    <tr>
-      <th> Name</th>
-    </tr>
-    <tr>
-      <td>
-        <GetCar/>
-      </td>
-    </tr>
-    <tr>
-      <td> mi</td>
-    </tr>
-    <tr>
-      <td> rahh</td>
-    </tr>
-  </table>
+  <div id = "header_container">
+    <h1> Welcome to my Website!</h1>
+  </div>
 );
 
-createRoot(document.getElementById('gojo')).render(
+createRoot(document.getElementById('root')).render(
   element
 );
