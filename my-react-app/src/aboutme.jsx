eed2f893231824cdcc72d21696b14466f93aca90
jyoshitanair <jyoshitanair123@gmail.com>
2026-08-29 17:25:53 -0700

    fixed seziding issues with the flashcards

--- my-react-app/src/aboutme.jsx
+++ my-react-app/src/aboutme.jsx
@@ -11,16 +11,18 @@ export default function aboutme() {
     <div>
         <h1> heyy! </h1>
         <div className = "button_holder">
-            <button style = {{fontSize: `${buttonT ? "larger": "smaller"}` }} className = "button_img" onClick = {() => setButtonSideT(!buttonT)}> {buttonT? "Speech" : "Certified Yapper. :P I like to do Informative Speaking(main) which is like yapping + artistic boards and SPAR(side) which is spontaneous speaking argumentation. I've even got up to the state level and won multiple online and TCFL competitions!"} {!buttonT? "": <img src = {speech}/>} </button>
-            <button style = {{fontSize: `${buttonF ? "larger": "smaller"}` }} className = "button_img" onClick = {() => setButtonSideF(!buttonF)}> {buttonF? "Art" : "I love to doodle and animate - especially my ocs! Feel free to reach out if you ever want something drawn. I have an art yt channel and back in the day I used to write/draw webtoons! Check out my "} {buttonF? "": <a href = "">Art Page</a>}  {!buttonF? "": <img src = {art}/>}</button>
-            <button style = {{fontSize: `${buttonA? "larger": "smaller"}` }} className = "button_img" onClick = {() => setButtonSideA(!buttonA)}> {buttonA? "Tech!" : "I'm pretty good with Godot Games, React, and Supabase! I also enjoy making hardware with arduino and I'm a member of Hack Club!"} {buttonA? "": <a href = ""> Check out my Projects Page! </a>} {!buttonA? "": <img src = {art}/>} </button>
+            <button style = {{fontSize: `${buttonT ? "larger": "3cqw"}`, textAlign: `${buttonT ? "center": "left"}` }} className = "button_img" onClick = {() => setButtonSideT(!buttonT)}> {buttonT? "Speech" : "Certified Yapper! I do Info and Spar mainly - and I've even got up to the state level and won multiple online and TCFL competitions! Check out my"} {buttonT? "": <a href = "">Speeches Page</a>} {!buttonT? "": <img src = {speech}/>} </button>
+            <button style = {{fontSize: `${buttonF ? "larger": "3cqw"}`, textAlign: `${buttonF ? "center": "left"}` }} className = "button_img" onClick = {() => setButtonSideF(!buttonF)}> {buttonF? "Art" : "I love to draw and animate my ocs! I have an art yt channel and sometimes draw webtoons! Check out my "} {buttonF? "": <a href = "">Art Page</a>}  {!buttonF? "": <img src = {art}/>}</button>
+            <button style = {{fontSize: `${buttonA? "larger": "3cqw"}`, textAlign: `${buttonA ? "center": "left"}` }} className = "button_img" onClick = {() => setButtonSideA(!buttonA)}> {buttonA? "Tech!" : "I use Godot, React, Arduino, and Supabase! also! I'm member of Hack Club! Check out my"} {buttonA? "": <a href = "">Projects Page!</a>} {!buttonA? "": <img src = {art}/>} </button>
         </div>
         <h2> Helloo ~ </h2>
-        <div className = "content_main" style = {{alignItems:"center", justifyContent:"center", gap: "30px", backgroundColor: "rgb(32, 165, 88)" }}>
-            <h2> Name: <span> <b> Jyoshita </b></span></h2>
-            <h2> Age: <span> <b> 16 </b></span></h2>
-            <h2> Likes: <span> <b> ??? </b></span> </h2>
-            <img className = "profile_img"src = {img}/>
+        <div className = "content_main" style = {{flexDirection: "row", alignItems:"center", justifyContent:"center", gap: "30px", backgroundColor: "rgb(32, 165, 88)" }}>
+            <div>
+                <h2> Hellooo~ I'm <span> <b> Jyoshita </b></span></h2>
+                <h2> Im a teen who loves <span> <b> tech </b></span></h2>
+                <h2> Want to leave a message? <span> <b> Click me! </b></span> </h2>
+            </div>
+            <img className = "change" src = {me}/>
         </div>
     </div>
 );
